aec4362945a2ff932d8c5969cf7bc9628356f8b6
Phil Everson <phil@phileverson.com>
2015-08-09 13:56:30 -0400

    Basically all of Part 1, completed. Somewhat in a jquery plugin format, spacing works, the last row works, and deals with plus/minus one rounding.

--- resize-gallery.js
+++ resize-gallery.js
@@ -8,27 +8,32 @@
 		* @param {number} spacing between images in a row, in pixels
 		* @returns {Array<Array<Object>>} array of rows of resized frames
 		*/
- 		layoutFrames: function(images, containerWidth, maxRowHeight, spacing) {
+ 		layoutFrames: function (images, containerWidth, maxRowHeight, spacing) {
 
- 			function resizeImageFromHeight (originalImage, newHeight) {
+ 			var layoutFrames = this;
+
+ 			layoutFrames.resizeImageFromHeight = function (originalImage, newHeight) {
 	 			var originalAspectRatio = originalImage.width / originalImage.height;
 	     		var adjustedImage = {
 	     			height: newHeight,
-	     			// width: Math.round(originalAspectRatio * newHeight),
 	     			width: (originalAspectRatio * newHeight),
 	     		};
 	     		return adjustedImage;
  			}
 
- 			function singleRowTooBig (row, maxWidth, spacing) {
+ 			layoutFrames.calculateRowWidth = function (row, spacing) {
  				var rowWidth = 0;
- 				// console.log('singleRowTooBig got a row array with x rows:' + row.length);
  				for (var i = 0; i < row.length; i++) {
  					rowWidth = row[i].width + rowWidth;
- 				};
- 				// console.log('the row is:');
- 				// console.log(row);
- 				console.log('When we ran singleRowTooBig, the total width was: ' + rowWidth);
+ 					if ((i + 1) < row.length) {
+ 						rowWidth = rowWidth + spacing;
+ 					}
+ 				}
+ 				return rowWidth;
+ 			}
+
+ 			layoutFrames.singleRowTooBig = function(row, maxWidth, spacing) {
+ 				rowWidth = this.calculateRowWidth(row, spacing);
  				if (rowWidth > maxWidth) {
  					return true;
  				}
@@ -37,73 +42,77 @@
  				}
  			}
 
- 			function resizeRowToContainerWidth (row, maxWidth, spacing) {
- 				while (singleRowTooBig(row, maxWidth, spacing)) {
- 					var newHeightForRow = row[0].height - 1; // pulling the frame height down by 1px
+ 			layoutFrames.resizeRowToContainerWidth = function(row, maxWidth, spacing) {
+ 				while (this.singleRowTooBig(row, maxWidth, spacing)) {
+ 					var newHeightForRow = row[0].height - 1; // pulling the frame height down by 1px until we hit an optimal solution
  					for (var i = 0; i < row.length; i++) {
- 						row[i] = resizeImageFromHeight(row[i], newHeightForRow);
+ 						row[i] = this.resizeImageFromHeight(row[i], newHeightForRow);
  					};
  				}
+
+ 				// Only rounding optimal set of widths to prevent false optimals
+ 				for (var i = 0; i < row.length; i++) {
+ 					row[i].width = Math.round(row[i].width);
+ 				};
+
+ 				// New row width, which accounts for the rounding done above
+ 				var rowWidthRounded = this.calculateRowWidth(row, spacing);
+
+ 				// Based on the rounded row width, how many pixels didn't we use?
+ 				var leftOverWidth = Math.round(maxWidth - rowWidthRounded);
+
+ 				var r = 0;
+ 				while (leftOverWidth > 0 && r < (row.length)) {
+ 					var plusMinusOneToAdd = Math.round(leftOverWidth / (row.length -r));
+ 					row[r].width = row[r].width + plusMinusOneToAdd;
+ 					leftOverWidth = leftOverWidth - plusMinusOneToAdd;
+ 					r++;
+ 				}
+
  				return row;
  			}
 
- 			var galleryRows = [];
- 			var singleRow = [];
+ 			layoutFrames.init = function (images, containerWidth, maxRowHeight, spacing) {
+	 			var galleryRows = [];
+	 			var singleRow = [];
 
- 			while (images.length > 0) {
+	 			while (images.length > 0) {
+	 				// Check if image greater than max height
+	 				var imageInQuestion = images.shift();
 
- 			console.log(''); 
- 			console.log(''); 
- 			console.log(''); 
- 			console.log(''); 
- 			console.log(''); 
- 			console.log(''); 
- 			console.log(''); 
- 			console.log(''); 
- 			console.log('');
- 			 
+	 				// add an image to our new row:
+	 				if (imageInQuestion.height >= maxRowHeight) {
+	 					var proposedImageSize = this.resizeImageFromHeight(imageInQuestion, maxRowHeight);
+	 					singleRow.push(proposedImageSize);
+	 				}
+	 				else {
+	 					singleRow.push(proposedImageSize);
+	 				}
 
- 				// Check if first image greater than max height
- 				var imageInQuestion = images.shift();
+	 				// Check if the width's of the images in our row are greater than the container
+	 				if (this.singleRowTooBig(singleRow, containerWidth, 10)) {
+	 					// scale the images back until they fit perfecly
+	 					var rowForContainer = this.resizeRowToContainerWidth(singleRow, containerWidth, 10);
+	 					// add the perfect row to our gallery
+	 					galleryRows.push(rowForContainer);
+	 					// reset singleRow so that we can start building a new row...
+	 					singleRow = [];
+	 				}
+	 			}
 
- 				// build row:
- 				if (imageInQuestion.height >= maxRowHeight) {
- 					var proposedImageSize = resizeImageFromHeight(imageInQuestion, maxRowHeight);
- 					singleRow.push(proposedImageSize);
- 				}
+	 			// Adding the last row, which may not be the complete width
+	 			if (singleRow.length > 0) {
+	 				galleryRows.push(singleRow);
+	 			}
+	 			console.log('***PERECT*** gallery is:');
+	 			console.log(galleryRows);
 
- 				// Check if row is too big
- 				if (singleRowTooBig(singleRow, containerWidth, 10)) {
- 					console.log('row IS TOO BIG');
- 					var rowForContainer = resizeRowToContainerWidth(singleRow, containerWidth, 10);
- 					console.log('******--------******--------******--------******')
- 					console.log('our row has now been resized, it is PERFECT. The frames are:')
- 					console.log(rowForContainer);
- 					console.log('******--------******--------******--------******')
- 					// adding our "perfect" row to the gallery
- 					galleryRows.push(rowForContainer);
- 					// resetting single row so that we can start building a new row...
- 					singleRow = [];
- 				}
- 				else {
- 					console.log('row is small'); // so stay in this while loop and keep growing...
- 				}
-
- 					// // Is new width greater than container?
- 					// if (proposedImageSize.width > container) {
- 					// 	console.log('width is bigger than container... do something?');
- 					// }
- 					// else {
-
- 					// 	singleRow.push(proposedImageSize);
- 					// }
+	 			for (var i = 0; i < galleryRows.length; i++) {
+	 				console.log('Gallery row #' + i + ' has a width of: ' + this.calculateRowWidth(galleryRows[i], spacing));
+	 			};
  			}
- 			console.log('FINAL, ***PERECT***, gallery is:');
- 			console.log(galleryRows);
 
- 			
-
+ 			this.init(images, containerWidth, maxRowHeight, spacing);
     	}
-    	
 	});
 })(jQuery);
